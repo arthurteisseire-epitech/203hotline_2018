9427bbd3dac001c882dff321fa29ceeee2c51ee6
arthur <arthur.teisseire@epitech.eu>
2019-03-11 12:15:54 +0100

    Add print formating

--- average.py
+++ average.py
@@ -1,3 +1,14 @@
 def average(d):
     pass
 
+
+def print_distribution(method_name, array, overload, compute_time):
+    print("%s distribution:", method_name)
+    tab = ""
+    for i in range(0, len(array)):
+        print(tab, end="")
+        print("%d -> %.3f" % (i, array[i]))
+        tab = "\t"
+    print("overload:  %.1f%%", overload)
+    print("computation time:  %.2f ms", compute_time)
+
