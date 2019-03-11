0030641decd99d08b98e72722bf28d49ea72bb51
arthur <arthur.teisseire@epitech.eu>
2019-03-11 12:41:42 +0100

    Fix lb

--- average.py
+++ average.py
@@ -4,7 +4,7 @@ import math
 def average(d):
     array = []
     for i in range(0, 50):
-        array.append(lb(i, 3500, d / 28800))
+        array.append(lb(3500, i, d / 28800))
     print_distribution("Binomial", array, 0, 0)
 
 
@@ -21,8 +21,6 @@ def print_distribution(method_name, array, overload, compute_time):
 
 
 def lb(n, k, p):
-    if k > n:
-        return 0
     proba_ok = pow(p, k)
     proba_ko = pow(1 - p, n - k)
     comb = math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
